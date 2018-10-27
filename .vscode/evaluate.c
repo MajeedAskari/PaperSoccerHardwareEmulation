46b01832df04c31a5bb9ecaba3faf67656cfe829
Majeed Askari <majeedaskari96@gmail.com>
2018-10-27 19:19:36 +0330

    evaluate.c

---
+++ .vscode/evaluate.c
@@ -0,0 +1,15 @@
+#include "evaluate.h"
+
+int evaluateGoal()
+{
+    if (checkGoal())
+    {
+        if(ballX == col/2 || ballX == col/2 + 1 || ballX == col/2 - 1){
+            if(ballY == 0)
+        }
+    }
+}
+
+int checkGoal()
+{
+}
