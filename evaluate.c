423922001859f712d1e8f67826cbd5fbe94e2f42
Majeed Askari <majeedaskari96@gmail.com>
2018-10-27 19:53:53 +0330

    changed check if statements place

--- evaluate.c
+++ evaluate.c
@@ -4,16 +4,17 @@ int evaluateState()
 {
     if (checkGoal())
     {
-        if (ballX == col / 2 || ballX == col / 2 + 1 || ballX == col / 2 - 1)
-        {
-            if ((ballY == 0 && !upSide) || (ballY == row - 1 && upSide))
-                return 999999;
-            if ((ballY == 0 && upSide) || (ballY == row - 1 && !upSide))
-                return -999999;
-        }
+
     }
 }
 
 int checkGoal()
 {
+    if (ballX == col / 2 || ballX == col / 2 + 1 || ballX == col / 2 - 1)
+    {
+        if ((ballY == 0 && !upSide) || (ballY == row - 1 && upSide))
+            return 999999;
+        if ((ballY == 0 && upSide) || (ballY == row - 1 && !upSide))
+            return -999999;
+    }
 }
