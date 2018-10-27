466b37386224a7cd51b08a6c0417da6a97e37d8c
Majeed Askari <majeedaskari96@gmail.com>
2018-10-27 20:55:37 +0330

    simple evaluateState implemented not tested

--- evaluate.c
+++ evaluate.c
@@ -2,10 +2,31 @@
 
 int evaluateState()
 {
-    if (checkGoal())
-    {
+    int Y = ballY;
+    int ballY = Y;
 
+    int g = checkGoal();
+    if (g == GOAL_VALUE || g == -GOAL_VALUE)
+        return g;
+
+    if (!upSide)
+    {
+        ballY = row - ballY;
     }
+
+    int eval = 0;
+    if (ballY == row - 2)
+    {
+        if (ballX == col / 2 || ballY == col / 2 - 1 || ballY == col / 2 + 1)
+            eval += 99999;
+    }
+    eval += (ballY - row / 2) * 5;
+    if (ballX <= col / 2)
+        eval += ballX * 5;
+    else
+        eval += (col - ballX) * 5;
+
+    return eval;
 }
 
 int checkGoal()
@@ -13,8 +34,8 @@ int checkGoal()
     if (ballX == col / 2 || ballX == col / 2 + 1 || ballX == col / 2 - 1)
     {
         if ((ballY == 0 && !upSide) || (ballY == row - 1 && upSide))
-            return 999999;
+            return GOAL_VALUE;
         if ((ballY == 0 && upSide) || (ballY == row - 1 && !upSide))
-            return -999999;
+            return -GOAL_VALUE;
     }
 }
--- evaluate.h
+++ evaluate.h
@@ -3,5 +3,10 @@
 #define EVALUATE_H
 
 #include "main.h"
+#define GOAL_VALUE 9999999
 
 #endif
+
+int evaluateState();
+
+int checkGoal();
