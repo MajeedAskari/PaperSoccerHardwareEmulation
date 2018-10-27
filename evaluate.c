97b17e3985185976dac611ea147a9fe59abdf63a
Majeed Askari <majeedaskari96@gmail.com>
2018-10-27 19:52:59 +0330

    checkGoal done not tested

---
+++ evaluate.c
@@ -0,0 +1,19 @@
+#include "evaluate.h"
+
+int evaluateState()
+{
+    if (checkGoal())
+    {
+        if (ballX == col / 2 || ballX == col / 2 + 1 || ballX == col / 2 - 1)
+        {
+            if ((ballY == 0 && !upSide) || (ballY == row - 1 && upSide))
+                return 999999;
+            if ((ballY == 0 && upSide) || (ballY == row - 1 && !upSide))
+                return -999999;
+        }
+    }
+}
+
+int checkGoal()
+{
+}
