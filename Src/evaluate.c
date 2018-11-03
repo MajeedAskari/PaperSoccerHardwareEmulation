ae7cbd513c71e5d7b7b04b0554a72ed8b2b07439
Majeed Askari <majeedaskari96@gmail.com>
2018-11-03 15:03:50 +0330

    fix evaluate function

--- Src/evaluate.c
+++ Src/evaluate.c
@@ -3,29 +3,27 @@
 
 int evaluateState()
 {
-    int Y = ballY;
-    int ballY = Y;
-
+    int ballX2 = ballX;
     int g = checkGoal();
     if (g == GOAL_VALUE || g == -GOAL_VALUE)
         return g;
 
     if (!upSide)
     {
-        ballY = row - ballY;
+        ballX2 = row - ballX2 - 1;
     }
 
     int eval = 0;
-    if (ballY == row - 2)
+    if (ballX2 == row - 2)
     {
-        if (ballX == col / 2 || ballY == col / 2 - 1 || ballY == col / 2 + 1)
+        if (ballY == col / 2 || ballY == col / 2 - 1 || ballY == col / 2 + 1)
             eval += 99999;
     }
-    eval += (ballY - row / 2) * 5;
-    if (ballX <= col / 2)
-        eval += ballX * 5;
-    else
-        eval += (col - ballX) * 5;
+    eval += (ballX2 - (row / 2)) * 5;
+    // if (ballY <= col / 2)
+    //     eval += ballY * 5;
+    // else
+    //     eval += (col - ballY) * 5;
 
     return eval;
 }
