df690f11f6cd2c3a9f1c3d20b713094267776d17
Majeed Askari <majeedaskari96@gmail.com>
2018-11-10 14:02:08 +0330

    fixed findMove

--- Src/move.c
+++ Src/move.c
@@ -7,49 +7,49 @@ int findMove()
     STACK_TYPE newNode;
     int children = 0;
 
-    if ((map[ballX][ballY - 1] & 0x01) == 0)
+    if ((map[ballX][ballY] & 0x10) == 0)
     {
         newNode.move = 0x01;
         sPush(newNode, currentStack);
         children++;
     }
-    if ((map[ballX + 1][ballY - 1] & 0x02) == 0)
+    if ((map[ballX][ballY] & 0x02) == 0)
     {
         newNode.move = 0x02;
         sPush(newNode, currentStack);
         children++;
     }
-    if ((map[ballX + 1][ballY] & 0x04) == 0)
+    if ((map[ballX][ballY] & 0x04) == 0)
     {
         newNode.move = 0x04;
         sPush(newNode, currentStack);
         children++;
     }
-    if ((map[ballX + 1][ballY + 1] & 0x08) == 0)
+    if ((map[ballX][ballY] & 0x08) == 0)
     {
         newNode.move = 0x08;
         sPush(newNode, currentStack);
         children++;
     }
-    if ((map[ballX][ballY + 1] & 0x10) == 0)
+    if ((map[ballX][ballY] & 0x01) == 0)
     {
         newNode.move = 0x10;
         sPush(newNode, currentStack);
         children++;
     }
-    if ((map[ballX - 1][ballY + 1] & 0x20) == 0)
+    if ((map[ballX][ballY] & 0x20) == 0)
     {
         newNode.move = 0x20;
         sPush(newNode, currentStack);
         children++;
     }
-    if ((map[ballX - 1][ballY] & 0x40) == 0)
+    if ((map[ballX][ballY] & 0x40) == 0)
     {
         newNode.move = 0x40;
         sPush(newNode, currentStack);
         children++;
     }
-    if ((map[ballX - 1][ballY - 1] & 0x80) == 0)
+    if ((map[ballX][ballY] & 0x80) == 0)
     {
         newNode.move = 0x80;
         sPush(newNode, currentStack);
